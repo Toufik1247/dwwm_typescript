774f91502f1d574bd9fb4dd86a36132a96a433ba
Toufik <said.toufik.simplon@gmail.com>
2023-03-08 12:33:00 +0100

    exercices2

---
+++ demo.ts
@@ -0,0 +1,20 @@
+// Système de constante et variable
+// Différent types
+
+const firstName: string = 'Kévin';
+const lastName: string = 'Wolff';
+
+const age: number = 30
+
+const isHeWakeUp: boolean = false;
+
+// Tableau
+const learners: any[] = ['Kevin', 30, true]
+
+// Objet
+const learner: any = {
+    name: 'Kevin',
+    age: 30,
+    wakeUp: false
+}
+
